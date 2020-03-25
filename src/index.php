d46763abfafc7032dd629dd92cfe841c9c022de4
Anuj Sharma <anuj_sharma401@yahoo.com>
2020-03-25 09:46:26 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Docker Swarm</p>
-                <p>The Kubernetes Docker container is running PHP version <?php echo phpversion(); ?>.</p>
+                <p>The Kubernetes Docker container is running on <?php echo gethostname() ?> with PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
